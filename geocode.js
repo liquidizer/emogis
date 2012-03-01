454875f8f62183900a1b97e47c07fdca71ff5f86
Stefan Dirnstorfer <stefandirnstorfer@gmail.com>
2012-03-01 10:44:28 +0000

    "Caching of geocodes"

--- geocode.js
+++ geocode.js
@@ -1,18 +1,78 @@
 var request= require('request');
+var fs= require('fs');
 
-function resolve(address, callback) {
-  var geo='http://maps.googleapis.com/maps/api/geocode/json?sensor=false&output=json&address='+escape(address);
+// location cache
+var locations= {};
+
+// load cache from disk
+fs.readFile('locations', function(err, data) {
+  if (err) {
+    console.log('Could not read location cache.');
+  } else {
+    var cacheFile= data.toString();
+    var lines= cacheFile.split('\n');
+    for (var i in lines) {
+      var match= lines[i].match(/(unknown|[0-9.,]+):(.*)/);
+      if (match) {
+        locations[match[2].toLowerCase()]= match[1];
+      }
+    }
+  }
+});
+
+// cache an address
+function codeToCache(address, location) {
+  if (locations[address.toLowerCase()]==location)
+    return;
+  locations[address.toLowerCase()]= location;
+  fs.readFile('locations', function(err, data) {
+    if (!err) {
+      var cacheFile= data.toString();
+      cacheFile+= '\n'+location+':'+address;
+      fs.writeFile('locations', cacheFile);
+    }
+  });
+}
+
+// Use Google to Resolve
+function resolveGoogle(address, callback) {
+  var geo='http://maps.googleapis.com/maps/api/geocode?sensor=false&output=json&address='+encodeURI(address);
+  console.log(address);
+  console.log(geo);
   request(geo, function(error, response, body) { 
     if (error) {
       callback(true);
     } else {
       var result= eval("("+body+")");
-      if (result.results.length>0)
-        callback(false, result.results[0].geometry.location);
-      else
+      if (result.status!="OK") {
+        console.log("Google geocode error: "+result.status);
+        callback(true);
+      }
+      if (result.results.length>0) {
+        var loc= result.results[0].geometry.location;
+        callback(false, loc.lat+','+loc.lng+',0');
+      } else
         callback(true);
     }
   });
 }
 
+function resolve(address, callback) {
+  var cached= locations[address.toLowerCase()];
+  if (cached) {
+    callback(false, cached);
+  } else {
+    resolveGoogle(address, function(error, location) {
+      if (error) {
+        console.log('noaddress: '+address);
+      } else {
+        callback(error, location);
+      }
+    });
+  }
+}
+
 exports.resolve= resolve;
+
+
+
--- ical.js
+++ ical.js
@@ -1,5 +1,6 @@
 var request= require('request');
 var ijp= require('./ijp');
+var geocode= require('./geocode');
 
 var url= 'http://events.piratenpartei-bayern.de/events/ical?gid=&cid=&subgroups=0&start=&end=';
 
@@ -7,13 +8,29 @@ function loadCalendar() {
   console.log('loading calendar');
     request(url, function(error, response, body) { 
     if (!error) {
+      body= body.replace(/\\/g,'');
       ijp.icalParser.parseIcal(body);
       var events= ijp.icalParser.ical.events;
-      for (var ev in events) {
-        console.log(events[ev]);
-      }
+      geoCodeEvents(events, 0, function() {
+      });
     }
   });
+}
+
+function geoCodeEvents(events, i, callback) {
+  if (i<events.length) {
+    if (!events[i].geo && events[i].location) {
+      geocode.resolve(events[i].location.value, function(error, address) {
+        if (!error) {
+          events[i].geo= { value: location };
+        }
+        geoCodeEvents(events, i+1, callback);
+      });
+    } else
+      geoCodeEvents(events, i+1, callback);
+  } else {
+    callback();
+  }
 }
 
 exports.loadCalendar= loadCalendar;
